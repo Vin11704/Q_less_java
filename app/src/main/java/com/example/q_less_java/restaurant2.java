fd10fa865c59198c9aa3357f0993f491cfd02a9a
Owen Sengdalavong <owentech4life22@gmail.com>
2024-04-03 11:26:11 +0800

    update restaurant 2 menus, allow scrollable and view cart button

---
+++ app/src/main/java/com/example/q_less_java/restaurant2.java
@@ -0,0 +1,14 @@
+package com.example.q_less_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class restaurant2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant2);
+    }
+}
